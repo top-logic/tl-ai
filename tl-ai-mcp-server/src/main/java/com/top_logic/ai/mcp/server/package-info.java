28d14bb814e4a8b138a9a172a58d12bd99d0567d
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-11-08 16:22:07 +0100

    Module setup.

---
+++ tl-ai-mcp-server/src/main/java/com/top_logic/ai/mcp/server/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Custom extensions for tl-ai-mcp-server.
+ */
+package com.top_logic.ai.mcp.server;
---
+++ tl-ai-mcp-server/src/test/java/com/top_logic/ai/mcp/server/TestApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 My Company. All Rights Reserved
+ */
+package com.top_logic.ai.mcp.server;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for the app <code>TL MCP Server</code>. 
+ */
+public class TestApp extends TestCase {
+	
+	public void testIt() {
+		// Add some test.
+	}
+	
+}
