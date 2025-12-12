f5294dd2c0992f1c244591f5ac8b6ab1184b9f03
Jonathan Hüsing <jonathan.huesing@top-logic.com>
2025-12-12 19:10:17 +0100

    Add Chat Module with base model.

---
+++ tl-chat/src/main/java/com/top_logic/chat/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Chat domain model for TopLogic applications.
+ *
+ * <p>
+ * This module provides the core chat functionality including:
+ * </p>
+ * <ul>
+ * <li>ChatRoot - Singleton container for all chats</li>
+ * <li>Chat - Individual chat sessions</li>
+ * <li>ChatMessage - Messages within a chat</li>
+ * </ul>
+ */
+package com.top_logic.chat;
